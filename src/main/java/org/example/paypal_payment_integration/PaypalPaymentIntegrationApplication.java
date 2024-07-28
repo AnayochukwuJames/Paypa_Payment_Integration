204e7dee452fb82d9dc75373c49f54271be2bca5
Anayochukwu James <anayochukwujames11@gmail.com>
2024-07-28 16:37:43 +0100

    First commit

---
+++ src/main/java/org/example/paypal_payment_integration/PaypalPaymentIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.example.paypal_payment_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalPaymentIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalPaymentIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/paypal_payment_integration/PaypalPaymentIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paypal_payment_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalPaymentIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
